43992141f769728441c714e3b5885d57600e94c8
lakshan <manualsplash@gmail.com>
2019-11-23 21:51:59 +0530

    Add resolvers

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
 import express from "express";
 import { ApolloServer, gql } from "apollo-server-express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 const schema = gql`
   type Query {
     me: User
+    user(id: ID!): User
+    users: [User!]
   }
   type User {
+    id: ID!
     username: String!
   }
 `;
+
+let users = {
+  1: {
+    id: "1",
+    username: "Robin Wieruch"
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids"
+  }
+};
+const me = users[1];
+
 const resolvers = {
   Query: {
+    user: (parent, { id }) => {
+      return users[id];
+    },
     me: () => {
-      return {
-        username: "Robin Wieruch"
-      };
+      return me;
+    }
+  },
+  User: {
+    username: parent => {
+      return parent.username;
     }
   }
 };
@@ -25,6 +49,7 @@ const server = new ApolloServer({
   typeDefs: schema,
   resolvers
 });
+
 server.applyMiddleware({ app, path: "/graphql" });
 app.listen({ port: 8000 }, () => {
   console.log("Apollo Server on http://localhost:8000/graphql");
